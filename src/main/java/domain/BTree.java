7ecda47844a73421d143e25d3185dce62f410e2b
Gilberth Chaves A <gchavesav@gmail.com>
2025-05-23 11:34:30 -0600

    Última actualización 23-05-2025

--- src/main/java/domain/BTree.java
+++ src/main/java/domain/BTree.java
@@ -27,7 +27,15 @@ public class BTree implements  Tree {
 
     @Override
     public boolean contains(Object element) throws TreeException {
-        return false;
+        if(isEmpty())
+            throw new TreeException("Binary Tree is empty");
+        return binarySearch(root, element);
+    }
+
+    private boolean binarySearch(BTreeNode node, Object element){
+        if(node==null) return false;
+        else if(util.Utility.compare(node.data, element)==0) return true;
+        return binarySearch(node.left, element) || binarySearch(node.right, element);
     }
 
     @Override
@@ -62,17 +70,123 @@ public class BTree implements  Tree {
 
     @Override
     public void remove(Object element) throws TreeException {
+        if(isEmpty())
+            throw new TreeException("Binary Tree is empty");
+        //Contribución de Jefferson Varela para que no borre repetidos. Excelente!!!
+        //para que el boolean funcione bien en el llamado recursivo, debe ir en un array
+        root = remove(root,element, new boolean[]{false});
+        //root = remove(root, element);
+    }
 
+    private BTreeNode remove(BTreeNode node, Object element, boolean[] deleted) throws TreeException{
+        if(node!=null){
+            if(util.Utility.compare(node.data, element)==0){
+                deleted[0] = true; // cambia a true porque lo va a eliminar
+
+                //caso 1. es un nodo si hijos, es una hoja
+                if(node.left==null && node.right==null) return null;
+                //caso 2-a. el nodo solo tien un hijo, el hijo izq
+                else if (node.left!=null&&node.right==null) {
+                    node.left = newPath(node.left, node.path);
+                    return node.left;
+                } //caso 2-b. el nodo solo tien un hijo, el hijo der
+                else if (node.left==null&&node.right!=null) {
+                    node.right = newPath(node.right, node.path);
+                    return node.right;
+                }
+                //caso 3. el nodo tiene dos hijos
+                else{
+                //else if (node.left!=null&&node.right!=null) {
+                    /* *
+                     * El algoritmo de supresión dice que cuando el nodo a suprimir
+                     * tiene 2 hijos, entonces busque una hoja del subarbol derecho
+                     * y sustituya la data del nodo a suprimir por la data de esa hoja,
+                     * luego elimine esa hojo
+                     * */
+                    Object value = getLeaf(node.right);
+                    node.data = value;
+                    node.right = removeLeaf(node.right, value, new boolean[]{false});
+                }
+            }
+            if(!deleted[0]) node.left = remove(node.left, element, deleted); //llamado recursivo por la izq
+            if(!deleted[0]) node.right = remove(node.right, element, deleted); //llamado recursivo por la der
+        }
+        return node; //retorna el nodo modificado o no
+    }
+
+    /* *
+     * Funciona cuando se invoca al metodo remove
+     * Sirve para actualizar los labels del nodo removido y sus
+     * descendientes (cuando aplica)
+     * */
+    private BTreeNode newPath(BTreeNode node,String label){
+        if(node!=null){
+            node.path = label;
+            node.left = newPath(node.left,label+"/left");
+            node.right = newPath(node.right,label+"/right");
+        }
+        return node;
+    }
+
+    private Object getLeaf(BTreeNode node){
+        Object aux;
+        if(node==null) return null;
+        else if(node.left==null&&node.right==null) return node.data; //es una hoja
+        else{
+            aux = getLeaf(node.left); //siga bajando por el subarbol izq
+            if(aux==null) aux = getLeaf(node.right);
+        }
+        return aux;
+    }
+
+    private BTreeNode removeLeaf(BTreeNode node, Object value, boolean[] deleted){
+        if(node==null) return null;
+        //si es una hoja y esa hoja es la que andamos buscando, la eliminamos
+        else if(node.left==null&&node.right==null&&util.Utility.compare(node.data, value)==0) {
+            deleted[0] = true; //el elemento fue eliminado
+            return null; //es una hoja y la elimina
+        }else{
+            node.left = removeLeaf(node.left, value, deleted);
+            if(!deleted[0]) node.right = removeLeaf(node.right, value, deleted);
+        }
+        return node; //retorna el subarbol derecho con la hoja eliminada
     }
 
     @Override
     public int height(Object element) throws TreeException {
-        return 0;
+        if(isEmpty())
+            throw new TreeException("Binary Tree is empty");
+        return height(root, element, 0);
+    }
+
+    //devuelve la altura de un nodo (el número de ancestros)
+    private int height(BTreeNode node, Object element, int level){
+        if(node==null) return 0;
+        else if(util.Utility.compare(node.data, element)==0) return level;
+        else return Math.max(height(node.left, element, ++level),
+                    height(node.right, element, level));
     }
 
     @Override
     public int height() throws TreeException {
-        return 0;
+        if(isEmpty())
+            throw new TreeException("Binary Tree is empty");
+        //return height(root, 0); //opción-1
+        return height(root); //opción-2
+    }
+
+    //devuelve la altura del árbol (altura máxima de la raíz a
+    //cualquier hoja del árbol)
+    private int height(BTreeNode node, int level){
+        if(node==null) return level-1;//se le resta 1 al nivel pq no cuente el nulo
+        return Math.max(height(node.left, ++level),
+                height(node.right, level));
+    }
+
+    //opcion-2
+    private int height(BTreeNode node){
+        if(node==null) return -1; //retorna valor negativo para eliminar el nivel del nulo
+        return Math.max(height(node.left), height(node.right)) + 1;
     }
 
     @Override
@@ -179,7 +293,7 @@ public class BTree implements  Tree {
         else {
 
         }
-        return "";
+        return ""; //corregir para el retorno correcto
     }
 
     public String printNodes2Children() throws TreeException {
--- src/test/java/domain/BTreeTest.java
+++ src/test/java/domain/BTreeTest.java
@@ -8,12 +8,53 @@ class BTreeTest {
     @Test
     void test() {
         BTree bTree = new BTree();
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 30; i++) {
             bTree.add(util.Utility.random(50));
         }
-        System.out.println(bTree);
+        System.out.println(bTree); //toString
         try {
-            System.out.println("BTree size: "+bTree.size());
+            System.out.println("Binary tree size: " + bTree.size()+"\nBinary tree height: " + bTree.height());
+            int deletedItems = 0;
+            System.out.println("\nBinary tree - contains an removed methods");
+            for (int i = 0; i < 20; i++) {
+                int value = util.Utility.random(50);
+                System.out.println(
+                        bTree.contains(value)
+                        ?"The value ["+value+"] exists in the binary tree" +". Height: "+bTree.height(value)
+                        :"The value ["+value+"]does not in the binary tree"
+                );
+                if(bTree.contains(value)){
+                    bTree.remove(value);
+                    deletedItems++;
+                    System.out.println("The value ["+value+"] has been removed");
+                }
+            }
+            System.out.println("Binary tree size: " + bTree.size()+"\nBinary tree height: " + bTree.height());
+            System.out.println("Deleted items: "+deletedItems+"\n");
+            System.out.println(bTree); //toString
+
+        } catch (TreeException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    void testHeight(){
+        BTree bTree = new BTree();
+        bTree.add(20); bTree.add(30); bTree.add(18); bTree.add(4); bTree.add(5);
+        bTree.add(50); bTree.add(70);
+        System.out.println(bTree); //toString
+        System.out.println();
+        try {
+            System.out.println("Binary tree - height(20): " + bTree.height(20));
+            System.out.println("Binary tree - height(30): " + bTree.height(30));
+            System.out.println("Binary tree - height(18): " + bTree.height(18));
+            System.out.println("Binary tree - height(4): " + bTree.height(4));
+            System.out.println("Binary tree - height(5): " + bTree.height(5));
+            System.out.println("Binary tree - height(50): " + bTree.height(50));
+            System.out.println("Binary tree - height(70): " + bTree.height(70));
+            System.out.println();
+            System.out.println("Binary tree - height(): " + bTree.height());
         } catch (TreeException e) {
             throw new RuntimeException(e);
         }
